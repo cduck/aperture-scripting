04e35b03b750145e0d86c64562a2c8a85934b5f9
Jerome Vuarand <jerome.vuarand@gmail.com>
2012-09-07 22:03:39 +0100

    Improved parameter parsing. Extended testing.

---
+++ Makefile
@@ -0,0 +1,68 @@
+GERBERS= \
+	../boards/gerber/hub.gbl \
+	../boards/gerber/hub.gbo \
+	../boards/gerber/hub.gbs \
+	../boards/gerber/hub.gml \
+	../boards/gerber/hub.gtl \
+	../boards/gerber/hub.gto \
+	../boards/gerber/hub.gts \
+	../boards/gerber/hub.txt \
+	../boards/gerber/motor3-jtag.gbl \
+	../boards/gerber/motor3-jtag.gbo \
+	../boards/gerber/motor3-jtag.gbs \
+	../boards/gerber/motor3-jtag.gml \
+	../boards/gerber/motor3-jtag.gtl \
+	../boards/gerber/motor3-jtag.gto \
+	../boards/gerber/motor3-jtag.gts \
+	../boards/gerber/motor3-jtag.txt \
+	../boards/gerber/motor3.gbl \
+	../boards/gerber/motor3.gbo \
+	../boards/gerber/motor3.gbs \
+	../boards/gerber/motor3.gml \
+	../boards/gerber/motor3.gtl \
+	../boards/gerber/motor3.gto \
+	../boards/gerber/motor3.gts \
+	../boards/gerber/motor3.txt \
+	../boards/gerber/motor7-jtag.gbl \
+	../boards/gerber/motor7-jtag.gbo \
+	../boards/gerber/motor7-jtag.gbs \
+	../boards/gerber/motor7-jtag.gml \
+	../boards/gerber/motor7-jtag.gtl \
+	../boards/gerber/motor7-jtag.gto \
+	../boards/gerber/motor7-jtag.gts \
+	../boards/gerber/motor7-jtag.txt \
+	../boards/gerber/motor7.gbl \
+	../boards/gerber/motor7.gbo \
+	../boards/gerber/motor7.gbs \
+	../boards/gerber/motor7.gml \
+	../boards/gerber/motor7.gtl \
+	../boards/gerber/motor7.gto \
+	../boards/gerber/motor7.gts \
+	../boards/gerber/motor7.txt \
+	../boards/gerber/sensor.gbl \
+	../boards/gerber/sensor.gbo \
+	../boards/gerber/sensor.gbs \
+	../boards/gerber/sensor.gml \
+	../boards/gerber/sensor.gtl \
+	../boards/gerber/sensor.gto \
+	../boards/gerber/sensor.gts \
+	../boards/gerber/sensor.txt \
+	../boards/gerber/panel.fab \
+	../boards/gerber/panel.gbl \
+	../boards/gerber/panel.gbo \
+	../boards/gerber/panel.gbs \
+	../boards/gerber/panel.gml \
+	../boards/gerber/panel.gtl \
+	../boards/gerber/panel.gto \
+	../boards/gerber/panel.gts \
+	../boards/gerber/panel.oln \
+	../boards/gerber/panel.placement.txt \
+	../boards/gerber/panel.sco \
+	../boards/gerber/panel.toollist.drl \
+	../boards/gerber/panel.txt
+
+.PHONY:test
+test:
+	grbcheck $(GERBERS)
+	lua test.lua
+
--- gerber.lua
+++ gerber.lua
@@ -26,96 +26,154 @@ local layer_polarities = {
 	C = 'clear',
 }
 
+local commands = {}
+
+function commands.AS(data, block) -- axis select
+	local a,b = block:match('^ASA(.)B(.)$')
+	assert(a and b and a~=b)
+	data.axis_select = { a = a, b = b }
+end
+
+function commands.FS(data, block) -- format statement
+	assert(#block % 2 == 0)
+	local omission,mode = block:match('^..(.)(.)')
+	local format = {
+		zero_omission = assert(zero_omissions[omission]),
+		coordinate_modes = assert(coordinate_modes[mode]),
+	}
+	for option,value in block:sub(5):gmatch('([A-Z])(%d+)') do
+		if option=='N' then
+			format.sequence_number = tonumber(value)
+		elseif option=='G' then
+			format.preparatory_function_code = tonumber(value)
+		elseif option=='X' then
+			local i,d = value:match('(.)(.)')
+			assert(i and d)
+			i,d = tonumber(i),tonumber(d)
+			format.x = {i=i, d=d}
+		elseif option=='Y' then
+			local i,d = value:match('(.)(.)')
+			assert(i and d)
+			i,d = tonumber(i),tonumber(d)
+			format.y = {i=i, d=d}
+		elseif option=='Z' then
+			local i,d = value:match('(.)(.)')
+			assert(i and d)
+			i,d = tonumber(i),tonumber(d)
+			format.z = {i=i, d=d}
+		elseif option=='D' then
+			format.draft_code = tonumber(value)
+		elseif option=='M' then
+			format.misc_code = tonumber(value)
+		else
+			error("unrecognized format statement option '"..option.."'")
+		end
+	end
+	data.format = format
+end
+
+commands.MI = nil -- mirror image
+
+function commands.MO(data, block) -- mode of units
+	data.unit_mode = assert(unit_modes[block:sub(3)])
+end
+
+function commands.OF(data, block) -- offset
+	assert(block=='OFA0B0')
+end
+
+commands.SF = nil -- scale factor
+
+function commands.IJ(data, block) -- image justify
+	assert(block=='IJALBL')
+end
+
+commands.IN = nil -- image name
+commands.IO = nil -- image offset
+
+function commands.IP(data, block) -- image polarity
+	data.image_polarity = assert(image_polarities[block:sub(3)])
+end
+
+commands.IR = nil -- image rotation
+commands.PF = nil -- plotter film
+
+function commands.AD(data, block) -- aperture description
+	-- :TODO:
+end
+
+function commands.AM(data, block) -- aperture macro
+	-- :TODO:
+end
+
+function commands.LN(data, block) -- layer name
+	-- discard anonymous layer if it's empty
+	if #data.layers == 1 and
+		not data.layers[1].name and
+		#data.layers[1] == 0 then
+		data.layers[1] = nil
+	end
+	local layer = {}
+	layer.name = block:sub(3)
+	table.insert(data.layers, layer)
+	data.state.layer = layer
+end
+
+function commands.LP(data, block) -- layer polarity
+	assert(data.state.layer, "LP before LN in "..data.state.filename)
+	data.state.layer.polarity = assert(layer_polarities[block:sub(3)])
+end
+
+commands.KO = nil -- knockout
+
+function commands.SR(data, block) -- step and repeat
+	assert(block=='SRX1Y1I0J0')
+end
+
+commands.SM = nil -- symbol mirror
+commands.IF = nil -- include file
+
 function _M.parse(filename)
 	local file = assert(io.open(filename, 'rb'))
 	local content = assert(file:read('*all'))
 	assert(file:close())
 	content = content:gsub('[%s%%]', '')
 	
-	local data = { layers = {} }
+	local data = { layers = { }, state = { filename = filename } }
+	
+	-- set a global anonymous layer
+	data.layers[1] = {}
+	data.state.layer = data.layers[1]
 	
 	local layer
 	
 	for block in content:gmatch('([^*]*)%*') do
-		if block:match('^ASA.B.$') then -- axis select
-			local a,b = block:match('^ASA(.)B(.)$')
-			assert(a and b and a~=b)
-			data.axis_select = { a = a, b = b }
-		elseif block:match('^FS') then -- format statement
-			assert(#block % 2 == 0)
-			local omission,mode = block:match('^..(.)(.)')
-			local format = {
-				zero_omission = assert(zero_omissions[omission]),
-				coordinate_modes = assert(coordinate_modes[mode]),
-			}
-			for option,value in block:sub(5):gmatch('([A-Z])(%d+)') do
-				if option=='N' then
-					format.sequence_number = tonumber(value)
-				elseif option=='G' then
-					format.preparatory_function_code = tonumber(value)
-				elseif option=='X' then
-					local i,d = value:match('(.)(.)')
-					assert(i and d)
-					i,d = tonumber(i),tonumber(d)
-					format.x = {i=i, d=d}
-				elseif option=='Y' then
-					local i,d = value:match('(.)(.)')
-					assert(i and d)
-					i,d = tonumber(i),tonumber(d)
-					format.y = {i=i, d=d}
-				elseif option=='Z' then
-					local i,d = value:match('(.)(.)')
-					assert(i and d)
-					i,d = tonumber(i),tonumber(d)
-					format.z = {i=i, d=d}
-				elseif option=='D' then
-					format.draft_code = tonumber(value)
-				elseif option=='M' then
-					format.misc_code = tonumber(value)
-				else
-					error("unrecognized format statement option '"..option.."'")
-				end
-			end
-			data.format = format
-	--	elseif block:match('^MI') then -- mirror image
-		elseif block:match('^MO') then -- mode of units
-			data.unit_mode = assert(unit_modes[block:sub(3)])
-	--	elseif block:match('^OF') then -- offset
-	--	elseif block:match('^SF') then -- scale factor
-		elseif block:match('^IJ') then -- image justify
-			assert(block=='IJALBL')
-	--	elseif block:match('^IN') then -- image name
-	--	elseif block:match('^IO') then -- image offset
-		elseif block:match('^IP') then -- image polarity
-			data.image_polarity = assert(image_polarities[block:sub(3)])
-	--	elseif block:match('^IR') then -- image rotation
-	--	elseif block:match('^PF') then -- plotter film
-		elseif block:match('^AD') then -- aperture description
-		elseif block:match('^AM') then -- aperture macro
-		elseif block:match('^LN') then -- layer name
-			layer = {}
-			layer.name = block:sub(3)
-			table.insert(data.layers, layer)
-		elseif block:match('^LP') then -- layer polarity
-			assert(layer)
-			layer.polarity = assert(layer_polarities[block:sub(3)])
-	--	elseif block:match('^KO') then -- knockout
-		elseif block:match('^SR') then -- step and repeat
-			assert(block=='SRX1Y1I0J0')
-	--	elseif block:match('^SM') then -- symbol mirror
-	--	elseif block:match('^IF') then -- include file
-		elseif block:match('^X%d') then
-		elseif block:match('^Y%d') then
-		elseif block:match('^I%d') then
-		elseif block:match('^J%d') then
+		local command = block:match('^[A-Z][A-Z]')
+		command = commands[command]
+		if command then
+			command(data, block)
+		elseif block:match('^[XYIJ]%d') then
+		elseif block:match('^D0[1-3]') then
 		elseif block:match('^D%d') then
+			local dcode = block:match('^D(%d+)')
+			assert(dcode)
+			dcode = tonumber(dcode)
+			assert(dcode >= 10 and dcode <= 999 and math.floor(dcode)==dcode)
+			data.state.aperture = block
 		elseif block:match('^G%d') then
+			local gcode,param = block:match('^G(%d+)(.*)$')
+			assert(gcode)
+			gcode = tonumber(gcode)
+		--	error("unsupported G-code "..gcode)
 		elseif block:match('^M%d') then
 		elseif block:match('^%d') then
 		else
 			error("block "..block.." not supported")
 		end
 	end
+	
+	-- discard the parsing state
+	data.state = nil
 	
 	return data
 end
