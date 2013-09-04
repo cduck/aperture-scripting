10d15cbe5734bb99fc50f3e0c5f4e18ae90023af
Jerome Vuarand <jerome.vuarand@gmail.com>
2013-09-04 01:01:38 +0100

    Moved outline extraction to grbv.

--- boards.lua
+++ boards.lua
@@ -120,83 +120,6 @@ end
 
 ------------------------------------------------------------------------------
 
-local function exterior(path)
-	local total = 0
-	for i=1,#path-1 do
-		local p0 = path[i-1] or path[#path-1]
-		local p1 = path[i]
-		local p2 = path[i+1] or path[1]
-		local dx1 = p1.x - p0.x
-		local dy1 = p1.y - p0.y
-		local dx2 = p2.x - p1.x
-		local dy2 = p2.y - p1.y
-		local l1 = math.sqrt(dx1*dx1+dy1*dy1)
-		local l2 = math.sqrt(dx2*dx2+dy2*dy2)
-		local angle = math.asin((dx1*dy2-dy1*dx2)/(l1*l2))
-		total = total + angle
-	end
-	return total > 0
-end
-
-local function find_outline(image)
-	-- :TODO: connect lines, since EAGLE generate discontinuous data
-	
-	-- find path with largest area
-	local amax,lmax,pmax = -math.huge
-	for l,layer in ipairs(image.layers) do
-		for p,path in ipairs(layer) do
-			local width = path.extents.right - path.extents.left
-			local height = path.extents.top - path.extents.bottom
-			local a = width * height
-			if a > amax then
-				amax,lmax,pmax = a,l,p
-			end
-		end
-	end
-	if not lmax or not pmax then return nil end
-	local path = image.layers[lmax][pmax]
-	-- check that the path has the same extents as the image
-	if path.extents.left ~= image.extents.left
-		or path.extents.right ~= image.extents.right
-		or path.extents.bottom ~= image.extents.bottom
-		or path.extents.top ~= image.extents.top then
-		return nil
-	end
-	-- check that the path is long enough to enclose a region
-	if #path < 3 then
-		return nil
-	end
-	-- check that the path is closed
-	if path[1].x ~= path[#path].x or path[1].y ~= path[#path].y then
-		return nil
-	end
-	-- check that path is a line, not a region
-	if not path.aperture then
-		return nil
-	end
-	-- :TODO: check that all other paths are within the outline
-	
-	-- convert to a region
-	local outline = { extents = path.extents }
-	for _,point in ipairs(path) do
-		table.insert(outline, point)
-	end
-	if not exterior(outline) then
-		local reverse = { extents = outline.extents }
-		for i=#outline,1,-1 do
-			table.insert(reverse, outline[i])
-		end
-		outline = reverse
-	end
-	
-	return outline,lmax,pmax
-end
-
-local ignore_outline = {
-	top_soldermask = true,
-	bottom_soldermask = true,
-}
-
 local function load_image(path, type)
 	print("loading "..tostring(path))
 	local image
@@ -251,14 +174,6 @@ local function load_image(path, type)
 		end
 	end
 	
-	if not ignore_outline[type] then
-		local outline,ilayer,ipath = find_outline(image)
-		if outline then
-			print("outline found")
-			image.outline = outline
-			image.layers[ilayer][ipath].outline = true
-		end
-	end
 	return image
 end
 
@@ -294,9 +209,6 @@ local function save_metadata(cache_directory, hash, image)
 	local metadata = {}
 	metadata.center_extents = image.center_extents
 	metadata.extents = image.extents
-	if image.outline then
-		metadata.outline = true
-	end
 	dump.tofile(metadata, tostring(cache_directory / (hash..'.lua')))
 end
 
@@ -310,7 +222,6 @@ local function load_metadata(cache_directory, hash, path, type)
 			hash = hash,
 			extents = region(metadata.extents),
 			center_extents = region(metadata.center_extents),
-			outline = metadata.outline,
 		}
 	else
 		return nil
