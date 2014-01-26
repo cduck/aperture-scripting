5a43ca05b523c44fab580fd96ebda8b9eb3364a4
Jerome Vuarand <jerome.vuarand@gmail.com>
2014-01-26 23:28:03 +0000

    Moved the code determining the center of a single-quadrant arc to a separate helper function.

--- boards/interpolation.lua
+++ boards/interpolation.lua
@@ -5,6 +5,42 @@ local table = require 'table'
 
 ------------------------------------------------------------------------------
 
+function _M.single_quadrant_center(x0, y0, i, j, x1, y1, interpolation)
+	local centers = {
+		{ x = x0 - i, y = y0 - j },
+		{ x = x0 + i, y = y0 - j },
+		{ x = x0 + i, y = y0 + j },
+		{ x = x0 - i, y = y0 + j },
+	}
+	local cx,cy
+	local best = math.huge
+	for _,c in ipairs(centers) do
+		local dxa,dya = x0 - c.x, y0 - c.y
+		local dxb,dyb = x1 - c.x, y1 - c.y
+		local ra = math.sqrt(dxa*dxa + dya*dya)
+		local rb = math.sqrt(dxb*dxb + dyb*dyb)
+		local ta = math.deg(math.atan2(dya, dxa))
+		local tb = math.deg(math.atan2(dyb, dxb))
+		local dt
+		if interpolation == 'clockwise' then
+			while ta <= tb do ta = ta + 360 end
+			dt = ta - tb
+		else
+			while tb <= ta do tb = tb + 360 end
+			dt = tb - ta
+		end
+		if dt < 180 then
+			local ratio = math.max(ra, rb) / math.min(ra, rb)
+			if ratio < best then
+				best = ratio
+				cx,cy = c.x,c.y
+			end
+		end
+	end
+	assert(cx and cy)
+	return cx,cy
+end
+
 function _M.interpolate(path, point)
 	local interpolation = point.interpolation
 	local quadrant = point.quadrant
@@ -15,38 +51,7 @@ function _M.interpolate(path, point)
 		local x0,y0 = point0.x, point0.y
 		local cx,cy
 		if quadrant == 'single' then
-			local centers = {
-				{ x = point0.x - point.i, y = point0.y - point.j },
-				{ x = point0.x + point.i, y = point0.y - point.j },
-				{ x = point0.x + point.i, y = point0.y + point.j },
-				{ x = point0.x - point.i, y = point0.y + point.j },
-			}
-			local best = math.huge
-			for _,c in ipairs(centers) do
-				local dxa,dya = point0.x - c.x, point0.y - c.y
-				local dxb,dyb = point.x - c.x, point.y - c.y
-				local ra = math.sqrt(dxa*dxa + dya*dya)
-				local rb = math.sqrt(dxb*dxb + dyb*dyb)
-				local ta = math.deg(math.atan2(dya, dxa))
-				local tb = math.deg(math.atan2(dyb, dxb))
-				local dt
-				if interpolation == 'clockwise' then
-					while ta <= tb do ta = ta + 360 end
-					dt = ta - tb
-				else
-					while tb <= ta do tb = tb + 360 end
-					dt = tb - ta
-				end
-			--	if dt <= 90 then
-				if dt < 180 then
-					local ratio = math.max(ra, rb) / math.min(ra, rb)
-					if ratio < best then
-						best = ratio
-						cx,cy = c.x,c.y
-					end
-				end
-			end
-			assert(cx and cy)
+			cx,cy = _M.single_quadrant_center(point0.x, point0.y, point.i, point.j, point.x, point.y, interpolation)
 		elseif quadrant == 'multi' then
 			cx,cy = point0.x + point.i, point0.y + point.j
 		else
