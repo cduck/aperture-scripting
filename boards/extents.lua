d9492ecc753cdca15629917fd9fdc4775a260983
Jerome Vuarand <jerome.vuarand@gmail.com>
2014-07-14 02:40:21 +0100

    The aperture paths are now generated only when requested, because some use cases can do without them, and that eliminates the dependency on a tesselation module. However this means board, outline, image and path extents no longer take apertures into account. Proper aperture extents without tesselation might be added in the future if needed.

--- boards/extents.lua
+++ boards/extents.lua
@@ -14,21 +14,12 @@ function _M.compute_aperture_extents(aperture)
 	return extents
 end
 
-function _M.compute_path_extents(path, apertures_extents)
-	if not apertures_extents then apertures_extents = {} end
-	local center_extents = region()
+function _M.compute_path_extents(path)
+	local extents = region()
 	for _,point in ipairs(path) do
-		center_extents = center_extents + point
+		extents = extents + point
 	end
-	local extents = region(center_extents)
-	local aperture = path.aperture
-	if aperture then
-		local aperture_extents = apertures_extents[aperture] or _M.compute_aperture_extents(aperture)
-		if not aperture_extents.empty then
-			extents = extents * aperture_extents
-		end
-	end
-	return extents,center_extents
+	return extents
 end
 
 function _M.compute_outline_extents(outline)
@@ -36,29 +27,14 @@ function _M.compute_outline_extents(outline)
 end
 
 function _M.compute_image_extents(image)
-	-- cache apertures extents for speedup
-	local apertures_extents = {}
-	local apertures = {}
-	for _,layer in ipairs(image.layers) do
-		for _,path in ipairs(layer) do
-			local aperture = path.aperture
-			if aperture and not apertures_extents[aperture] then
-				apertures_extents[aperture] = _M.compute_aperture_extents(aperture)
-			end
-		end
-	end
-	
-	-- compute extents
-	local center_extents = region()
 	local extents = region()
 	for _,layer in ipairs(image.layers) do
 		for _,path in ipairs(layer) do
-			local path_extents,path_center_extents = _M.compute_path_extents(path, apertures_extents)
-			center_extents = center_extents + path_center_extents
+			local path_extents = _M.compute_path_extents(path)
 			extents = extents + path_extents
 		end
 	end
-	return extents,center_extents
+	return extents
 end
 
 function _M.compute_board_extents(board)
